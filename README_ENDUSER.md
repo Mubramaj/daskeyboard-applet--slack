861b7f6f45bb12e8c4ecd8a2cea0e1b488c30e3a
F-DALLA-VALLE <dallavalle.fabien@gmail.com>
2019-03-27 11:56:50 -0500

    First commit

---
+++ README_ENDUSER.md
@@ -0,0 +1,9 @@
+# Slack
+
+Displays Slack premium account notifications on a Das Keyboard Q Series.
+
+Slack is a collaboration hub, where the right people and the right
+information come together, helping everyone get work done.
+For more information about Slack visit <https://www.slack.com>.
+
+![Slack applet on a Das Keyboard Q](assets/image.png "Das Keyboard Slack applet")
---
+++ index.js
@@ -0,0 +1,81 @@
+const q = require('daskeyboard-applet');
+
+const logger = q.logger;
+const queryUrlBase = '	https://slack.com/api/';
+
+function getTimestamp() {
+  var d = new Date(Date.now()),
+      month = '' + (d.getMonth() + 1),
+      day = '' + d.getDate(),
+      year = d.getFullYear();
+
+  if (month.length < 2) month = '0' + month;
+  if (day.length < 2) day = '0' + day;
+
+  return [year, month, day].join('-');
+}
+
+class Slack extends q.DesktopApp {
+  constructor() {
+    super();
+    this.timestamp = getTimestamp();
+    // For checking plural or singular
+    this.notification = "";
+  }
+
+  async getMessages() {
+    const query = "search.messages?query=pickleface&sort=timestamp&pretty=1";
+    const proxyRequest = new q.Oauth2ProxyRequest({
+      apiKey: this.authorization.apiKey,
+      uri: queryUrlBase + query
+    });
+
+    // first get the user projects
+    return this.oauth2ProxyRequest(proxyRequest);
+  }
+
+  async run() {
+    console.log("Running.");
+    return this.getMessages().then(newMessages => {
+      this.timestamp = getTimestamp();
+      logger.info("This is the response",newMessages);
+      if (newMessages && newMessages.length > 0) {
+
+        if (newMessages.length == 1) {
+          this.notification = "notification";
+        } else {
+          this.notification = "notifications";
+        }
+
+        logger.info("Got " + newMessages.length + this.notification);
+
+
+        return new q.Signal({
+          points: [
+            [new q.Point("#0000FF", q.Effects.BLINK)]
+          ],
+          name: `Slack`,
+          message: `You have a new notification.`,
+          link: {
+            url: 'https://slack.com',
+            label: 'Show in Slack',
+          },
+        });
+      } else {
+        return null;
+      }
+    }).catch(error => {
+      const message = error.statusCode == 402
+        ? 'Payment required. This applet requires a premium Slack account.' : error;
+      logger.error(`Sending error signal: ${message}`);
+      throw new Error(message);
+    })
+  }
+}
+
+
+module.exports = {
+  Slack: Slack
+}
+
+const applet = new Slack();
